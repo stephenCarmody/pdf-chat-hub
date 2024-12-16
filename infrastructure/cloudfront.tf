05ae2a59f5bc58211b8890fffda38baf546255f8
stephenCarmody <stephen.m.carmody@gmail.com>
2024-12-16 12:02:30 +0100

    feat: added domain

--- infrastructure/cloudfront.tf
+++ infrastructure/cloudfront.tf
@@ -44,8 +44,39 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 
   viewer_certificate {
-    cloudfront_default_certificate = true
+    acm_certificate_arn = aws_acm_certificate.cert.arn
+    ssl_support_method = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
   }
 
   price_class = "PriceClass_100"  // Use only North America and Europe
+  aliases = ["www.pdfchathub.com"]
+}
+
+# For ACM certificate
+resource "aws_acm_certificate" "cert" {
+  provider = aws.us-east-1  # Important: Certificate must be in us-east-1 for CloudFront
+  domain_name = "www.pdfchathub.com"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Output the validation records so you can add them to Namecheap manually
+output "certificate_validation_records" {
+  value = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      value  = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+}
+
+# Provider configuration
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
 }
